711e713de7c0f3510f1f036814785293fd905a83
Ilya Troy <troytft@gmail.com>
2019-11-03 14:28:03 +0300

    fix tests

--- src/Transformer/BooleanTransformer.php
+++ src/Transformer/BooleanTransformer.php
@@ -3,13 +3,14 @@
 namespace Mapper\Transformer;
 
 use Mapper\Exception\Transformer\BooleanRequiredException;
+use function is_bool;
 
 class BooleanTransformer implements TransformerInterface
 {
     public function transform($value, array $options = [])
     {
         $value = filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
-        if ($value === null) {
+        if (!is_bool($value)) {
             throw new BooleanRequiredException();
         }
 
